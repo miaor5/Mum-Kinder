227e1d1ebed7afdcf2567803f842c6fb7cf03222
María Ortiz <99879098+miaor5@users.noreply.github.com>
2024-05-05 02:02:17 +0200

    add images

--- src/components/card.js
+++ src/components/card.js
@@ -3,6 +3,7 @@ function Card(props) {
     <>
       <img src={props.image} alt={props.alt} />
       <p>Up to 40 % Off</p>
+      <p>Selected {props.category}</p>
       <a href="">SHOP NOW</a>
     </>
   );
